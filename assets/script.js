584afa951ceda2f78547198096c063dcf32b9d8d
ashley lockhart <allockhart49@gmail.com>
2022-02-05 13:50:45 -0700

    starting js

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,12 @@
 
-
+ const inputEl = document.getElementById("city-input");
+ const searchEl = document.getElementById("search-button");
+ const nameEl = document.getElementById("city-name");
+ const currentTempEl = document.getElementById("temperature");
+ const currentHumidityEl = document.getElementById("humidity");
+ const currentWindEl = document.getElementById("wind-speed");
+ const currentUVEl = document.getElementById("UV-index");
+ const historyEl = document.getElementById("history");
+  
 
 
