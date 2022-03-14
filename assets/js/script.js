c6c652029974a3bb7f8dba3f01aa7dc9880cd0b6
ashley lockhart <allockhart49@gmail.com>
2022-03-14 17:37:12 -0600

    starting new

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,36 @@
+
+var today = moment().format("L");
+console.log(today);
+var today = moment().format("L");
+console.log(today);
+
+var cities = [];
+var cityInputEl = document.getElementById("cityInput");
+var weatherContentEl = document.getElementById("weatherContent");
+var citySearchEl = document.getElementById("searchCity");
+var weather5DayEl = document.getElementById("fiveDay");
+var searchHistoryEl = document.getElementById("History");
+var currentWeatherEl = document.getElementById("currentWeather");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var getCityWeather = function (city) {
   var apiKey = "08ef12578be8b6481d16de9c739f47a8";
   var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`; 
